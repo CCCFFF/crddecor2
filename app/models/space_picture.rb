14f99f7173783e3d924f0f25fe0f9be49c35d957
Craig <craig.frey@hotmail.com>
2013-09-12 15:06:30 -0500

    fixed index page

--- app/models/space_picture.rb
+++ app/models/space_picture.rb
@@ -2,7 +2,7 @@ class SpacePicture < ActiveRecord::Base
   belongs_to :space
   has_many :thoughts
 
-    has_attached_file :image, :styles => { :medium => "200x200>" }
+  has_attached_file :image, :styles => { :thumb => "200x200>" }
 
   validates_attachment :image, content_type: {content_type: ['image/jpeg', 'image/jpg', 'image/png', 'image/gif'] }, size: {less_than: 2.megabytes}
 end
